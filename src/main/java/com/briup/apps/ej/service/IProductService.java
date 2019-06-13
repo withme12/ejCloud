06920ad4627d7a062f9bd7af2daa2a9272d8563f
lx642148816 <642148816@qq.com>
2019-06-13 10:51:38 +0800

    Product

--- src/main/java/com/briup/apps/ej/service/IProductService.java
+++ src/main/java/com/briup/apps/ej/service/IProductService.java
@@ -1,13 +1,20 @@
 package com.briup.apps.ej.service;
 
 import com.briup.apps.ej.bean.Product;
-
+import com.briup.apps.ej.bean.ProductExample;
+import com.briup.apps.ej.utils.Message;
 import java.util.List;
 
 public interface IProductService {
-    int deleteById(Long id);
+    List<Product> findAll();
 
+
+//详细信息查看
     Product selectById(Long id);
-
+//产品信息更改或者新增产品信息
+    int saveOrUpdate(Product product) throws Exception;
+//单个删除
+    int deleteById(Long id) throws  Exception;
+ //
 
 }
--- src/main/java/com/briup/apps/ej/service/impl/ProductServiceImpl.java
+++ src/main/java/com/briup/apps/ej/service/impl/ProductServiceImpl.java
@@ -1,4 +1,53 @@
 package com.briup.apps.ej.service.impl;
 
-public class ProductServiceImpl {
+import com.briup.apps.ej.bean.Product;
+import com.briup.apps.ej.bean.ProductExample;
+import com.briup.apps.ej.dao.ProductMapper;
+import com.briup.apps.ej.service.IProductService;
+
+import org.springframework.stereotype.Service;
+import java.util.List;
+import javax.annotation.Resource;
+
+@Service 
+public class ProductServiceImpl implements IProductService{
+    @Resource
+    private ProductMapper productMapper;
+
+    @Override
+    public List<Product> findAll(){
+        ProductExample example = new ProductExample();
+        return productMapper.selectByExample(example);
+    }
+
+    //详细信息查看（通过id）
+    @Override
+    public Product selectById(Long id){
+        return productMapper.selectByPrimaryKey(id);
+
+    }
+
+    //产品信息更改或者新增产品信息
+    @Override
+    public int saveOrUpdate(Product product){
+        //id自动生成
+        if(product.getId()== null) {
+            product.setStatus("正常");
+            return productMapper.insert(product);
+        }else {
+            return productMapper.updateByPrimaryKey(product);
+        }
+
+    }
+
+    //单个删除(通过id)
+    @Override
+    public int deleteById(Long id) throws Exception{
+        if (id == null) {
+            throw new Exception("要删除的产品不存在");
+        } else {
+            return productMapper.deleteByPrimaryKey(id);
+        }
+    }
+
 }
